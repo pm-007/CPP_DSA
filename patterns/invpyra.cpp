91ce187d83c72bc0c125763aa476cf9537b68682
pm-007 <priyanshm042@gmail.com>
2025-08-13 19:27:48 +0530

    inverted pyramid

---
+++ patterns/invpyra.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;cout<<"enter the number: ";cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"  ";
+        }
+        for (int k=0;k<2*(n-i)-1;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+}
